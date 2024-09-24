8ff7c887a0653fe8d2f6a2594fb44e00ab9065b3
KwonGreenTea <bnzzzang@gmail.com>
2024-09-25 01:00:49 +0900

    초기 controller 맵핑 재수정

--- src/main/java/com/hyfocus/web/HomeController.java
+++ src/main/java/com/hyfocus/web/HomeController.java
@@ -2,6 +2,7 @@ package com.hyfocus.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
@@ -13,7 +14,7 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping("/HYFOCUS")
+	@RequestMapping(value = "/HYFOCUS", method = RequestMethod.GET)
 	public String home(RedirectAttributes reAttr) {
 		return "redirect:/main";
 	}
