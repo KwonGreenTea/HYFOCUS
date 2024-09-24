a6752809576f76133fc87edad3a727f279a19907
KwonGreenTea <bnzzzang@gmail.com>
2024-09-25 03:57:45 +0900

    java version for 10

--- src/main/java/com/hyfocus/web/HomeController.java
+++ src/main/java/com/hyfocus/web/HomeController.java
@@ -14,7 +14,7 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/HYFOCUS", method = RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(RedirectAttributes reAttr) {
 		return "redirect:/main";
 	}
