07d42af5f8fb8fc22db087ceff58bde74656a28e
KwonGreenTea <bnzzzang@gmail.com>
2025-03-23 15:10:47 +0900

    웹 소켓 삭제

--- src/main/java/com/hyfocus/web/config/ServletConfig.java
+++ src/main/java/com/hyfocus/web/config/ServletConfig.java
@@ -1,6 +1,5 @@
 package com.hyfocus.web.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,16 +7,12 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-
-import com.hyfocus.web.util.WebSocketHandler;
 
 // servlet-context.xml과 동일
 @Configuration // Spring Container에서 관리하는 설정 클래스
 @EnableWebMvc // Spring MVC 기능 사용
 @ComponentScan(basePackages = { "com.hyfocus.web" }) // component scan 설정
-public class ServletConfig implements WebMvcConfigurer, WebSocketConfigurer {
+public class ServletConfig implements WebMvcConfigurer {
 
 	// ViewResolver 설정 메소드
 	@Override
@@ -33,16 +28,6 @@ public class ServletConfig implements WebMvcConfigurer, WebSocketConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		// resources 디렉토리 설정
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-	}
-	
-	@Override
-	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-		registry.addHandler(socketHandler(), "/rentSuccess").setAllowedOrigins("http://www.hyfocus.xyz");
-	}
-
-	@Bean
-	public WebSocketHandler socketHandler() {
-		return new WebSocketHandler();
 	}
 
 } // end ServletConfig
